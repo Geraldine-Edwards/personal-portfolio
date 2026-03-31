07c218ac519ecbf06f6bffe62f8f0b41b44faba8
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2026-03-31 16:25:37 +0100

    refactor: add comment to clarify generic type parameter in useFocusOnOpen hook

--- src/hooks/UseFocusOnOpen.ts
+++ src/hooks/UseFocusOnOpen.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 
+// T is a generic type parameter that must extend a HTMLElement
 function useFocusOnOpen<T extends HTMLElement>(
   ref: React.RefObject<T | null>,
   isOpen: boolean
